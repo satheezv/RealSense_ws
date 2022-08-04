69c725e7ce20b3908964c4e45adbeef02ca280a1
satheezv <sathiz.v@gmail.com>
2022-08-04 14:33:22 +0100

    initial commit of realsense_ws

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/satheeshkumar/RealSense_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/satheeshkumar/RealSense_ws/devel;/opt/ros/noetic'.split(';') if '/home/satheeshkumar/RealSense_ws/devel;/opt/ros/noetic' != '' else []
